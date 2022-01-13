a6d0523cbf07d672510c4d4e99fa00e220624194
Lss0020 <lss0020@mix.wvu.edu>
2022-01-13 17:31:13 -0500

    <Lss0020> Task 01 upload b

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -1,3 +1,40 @@
 #install.packages("swirl")
 library(swirl)
-
+swirl ()
+Sian
+1
+skip ()
+1
+1
+1
+5 + 7
+x <- 5 + 7
+x
+y <- x - 3
+y
+z <- c(1.1,9,3.14)
+?c
+?z
+c(z,555,z)
+z * 2 + 100
+my_sqrt <- sqrt(z-1)
+2
+3
+1
+2
+my_sqrt
+my_div <- z / my_sqrt
+2
+my_div
+c(1,2,3,4) + c(0,10)
+c(1,2,3,4) + c(0,10,100)
+info ()
+(z * 2 + 100)
+skip ()
+my_div
+1
+lss0020@mix.wvu.edu
+skip ()
+1
+Lss0020
+0
