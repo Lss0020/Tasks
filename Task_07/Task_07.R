7e0fa6257d71992e553e58efc697216ba6b64ae5
Lss0020 <lss0020@mix.wvu.edu>
2022-02-24 12:47:33 -0500

    <Lss0020> Task 07

---
+++ Task_07/Task_07.R
@@ -0,0 +1,8 @@
+install.packages("rvertnet")
+setwd('C:\\Users\\sians\\AppData\\Local\\Temp\\RtmpGM69AS\\downloaded_packages')
+library(rvertnet)
+"rvertnet-packages"
+?revertnet
+?VertNet
+vertsearch(taxon = "reptiles", state = "hawaii")
+traitsearch(taxon = "reptiles", has_sex = TRUE)
