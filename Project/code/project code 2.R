ef6f1e15b7cda93797716d8ebc453f65e83e46e3
Lss0020 <lss0020@mix.wvu.edu>
2022-04-29 10:17:46 -0400

    Project

--- Project/code/project code 2.R
+++ Project/code/project code 2.R
@@ -13,18 +13,15 @@ ggscatter(headmorphdata, x = "TEETH.NUMBER", y = "HEAD.WIDTH",
 Spec <- unique(headmorphdata[,1])
 
 par(mar=c(4,4,1,1))
-plot(headmorphdata[which(headmorphdata[,1]==Spec[1]),2], headmorphdata[which(headmorphdata[,1]==Spec[1]),3], col='orange', xlab="Teeth number", ylab="Head width(mm)")
+plot(headmorphdata[which(headmorphdata[,1]==Spec[1]),2], headmorphdata[which(headmorphdata[,1]==Spec[1]),3], col='orange', xlab="Teeth number", ylab="Head width(mm)", ylim=c(0,25))
 points(headmorphdata[which(headmorphdata[,1]==Spec[2]),2], headmorphdata[which(headmorphdata[,1]==Spec[2]),3], col='blue', pch=16, ylab="Head width", xlab="")
-legend("topright", legend=c("T. Mel No eat crayfish", "T. Mel eat crayfish"), text.col=c("orange", "blue"), bty="n")
-linek <- lm(y~x)
+legend("topright", legend=c("T. Mel No eat hard crayfish", "T. Mel eat hard crayfish"), text.col=c("orange", "blue"), bty="n")
+
+linek <- lm(headmorphdata[which(headmorphdata[,1]==Spec[1]),3] ~ headmorphdata[which(headmorphdata[,1]==Spec[1]),2])
 abline(linek, col="red")
+abline
 
 abline(Model1, col="red")
-title("Correlation Between T. Mel According to Teeth Number vs. Head Width(mm)")
+title("Correlation Between T. Mel. According to Teeth Number vs. Head Width(mm)", size= "1.5")
 abline(lm())
-
-install.packages("ggpubr")
-library(phytools)
-trees <- read.nexus("headmorphdata")
-data(headmorphdata)
 
